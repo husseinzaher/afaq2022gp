0bbc92ebaf369a3b821f88eca0616e965ec6091d
Hussein Zaher <husseinzaher@outlook.com>
2022-03-09 01:17:13 +0200

    campaign-news

--- app/Http/Controllers/FrontendController/HomeController.php
+++ app/Http/Controllers/FrontendController/HomeController.php
@@ -15,6 +15,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('frontend.layouts.master');
+        return view('frontend.index');
     }
 }
